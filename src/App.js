15c2c5dea8f15502463db6d0f31451f1b16e4ed7
mannycolon <colonmanuel7@gmail.com>
2019-08-14 23:51:30 -0400

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React, { Component } from 'react';
-import fs from 'fs'
+import React, { useState } from 'react';
 
-export default class App extends Component {
-  render() {
-    const exists = fs.existsSync('src/index.js')
-    console.log('exists', exists)
-    
-    return (
-      <h1>Hello, q world.</h1>
-    );
-  }
+export default function App() {
+  const [isLoginOpened, openLogin] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <div>
+      {isLoginOpened ?
+        <form
+          method="post"
+          onSubmit={async e => {
+            e.preventDefault()
+            alert(`You're logged in as ${email}`)
+          }}
+        >
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}/>
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}/>
+          </label>
+          <button type="submit">Sign In!</button>
+        </form>
+      :
+        <button
+          id="go-to-login"
+          onClick={() => openLogin(true)}
+        >
+          Go to Login
+        </button>
+      }
+    </div>
+  );
 }
